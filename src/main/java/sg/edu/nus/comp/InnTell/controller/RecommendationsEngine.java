e0579ff3961904e999e4d4ccdd5f8f73c83083b4
Swaminathan Venkatraman <swamvk@gmail.com>
2018-03-25 18:09:12 +0800

    Fixed null pointer exception

--- src/main/java/sg/edu/nus/comp/InnTell/controller/RecommendationsEngine.java
+++ src/main/java/sg/edu/nus/comp/InnTell/controller/RecommendationsEngine.java
@@ -37,11 +37,11 @@ public class RecommendationsEngine {
 		return recommendation;
 	}
 
-	public static void main(String args[]) {
+	/*public static void main(String args[]) {
 		RecommendationsEngine r = new RecommendationsEngine();
 		for (int i = 1; i <= 12; i++) {
 			System.out.println(i);
 		}
-	}
+	}*/
 
 }
